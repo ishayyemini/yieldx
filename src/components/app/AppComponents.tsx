ffe04e6a7c8d87be8b065b1f84a337c40f66ca58
Ishay Yemini <ishay.yemini@gmail.com>
2022-05-10 16:34:03 +0300

    Use mocked charts

--- src/components/app/AppComponents.tsx
+++ src/components/app/AppComponents.tsx
@@ -5,9 +5,21 @@ import {
   Card,
   CardBody,
   CardHeader,
-  Paragraph,
   Text,
 } from 'grommet'
+import {
+  BarElement,
+  CategoryScale,
+  Chart as ChartJS,
+  ChartData,
+  Legend,
+  LinearScale,
+  Title,
+  Tooltip,
+} from 'chart.js'
+import { Bar } from 'react-chartjs-2'
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
 
 const CardWrapper: FC<BoxExtendedProps> = ({ direction = 'row', ...props }) => {
   return <Box direction={direction} fill {...props} />
@@ -16,13 +28,18 @@ const CardWrapper: FC<BoxExtendedProps> = ({ direction = 'row', ...props }) => {
 type GraphCardProps = {}
 
 const ChartCard: FC<GraphCardProps & BoxExtendedProps> = () => {
+  const data: ChartData<'bar'> = {
+    labels: ['First', 'Second'],
+    datasets: [{ label: 'Hey!', data: [20, 10] }],
+  }
+
   return (
     <Card flex>
       <CardHeader>
         <Text weight={'bold'}>Header</Text>
       </CardHeader>
       <CardBody>
-        <Paragraph>fjidjfoisj</Paragraph>
+        <Bar data={data} />
       </CardBody>
     </Card>
   )
