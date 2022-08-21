a281ce317f0a7300183d93c7cd3476f4620281c0
Ishay Yemini <ishay.yemini@gmail.com>
2022-08-21 19:27:43 +0200

    Not working DB connection

--- src/components/LabelTrolleys.tsx
+++ src/components/LabelTrolleys.tsx
@@ -1,6 +1,8 @@
 import { FC, useCallback } from 'react'
-import { Box, Button, DateInput, FormField, TextInput } from 'grommet'
+import { Box, Button, DateInput, FormField, Main, TextInput } from 'grommet'
 import { useForm } from 'react-hook-form'
+import queryString from 'query-string'
+import styled from 'styled-components'
 
 type TrolleyFormType = {
   flock: string
@@ -8,6 +10,12 @@ type TrolleyFormType = {
   date: string
   wh: string
 }
+
+const Wrapper = styled(Box).attrs({
+  background: { light: 'brand' },
+  pad: 'small',
+  round: 'small',
+})``
 
 const LabelTrolleys: FC = () => {
   const { register, handleSubmit, watch, setValue } = useForm({
@@ -22,35 +30,49 @@ const LabelTrolleys: FC = () => {
   const watchDate = watch('date')
 
   const onSubmit = useCallback((values: TrolleyFormType) => {
-    console.log(values)
+    fetch(
+      'https://ls72mt05m4.execute-api.us-east-1.amazonaws.com/dev/default?' +
+        queryString.stringify({ ...values, db: 'ishay' })
+    )
+      .then((res) => res.json())
+      .then((res) => console.log(res))
   }, [])
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Box>
-        <FormField label={'Label *'} required>
-          <TextInput {...register('label')} />
-        </FormField>
-        <FormField label={'Flock *'} required>
-          <TextInput {...register('flock')} />
-        </FormField>
-        <FormField label={'Warehouse'}>
-          <TextInput {...register('wh')} />
-        </FormField>
-        <FormField label={'Date'}>
-          <DateInput
-            {...register('date')}
-            format={'mm/dd/yyyy'}
-            value={watchDate}
-            onChange={({ value }) => {
-              if (typeof value === 'string') setValue('date', value)
-            }}
-          />
-        </FormField>
+    <Main align={'center'} justify={'center'}>
+      <Wrapper>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormField label={'Label *'} required>
+            <TextInput {...register('label')} />
+          </FormField>
+          <FormField label={'Flock *'} required>
+            <TextInput {...register('flock')} />
+          </FormField>
+          <FormField label={'Warehouse'}>
+            <TextInput {...register('wh')} />
+          </FormField>
+          <FormField label={'Date'}>
+            <DateInput
+              {...register('date')}
+              format={'mm/dd/yyyy'}
+              value={watchDate}
+              onChange={({ value }) => {
+                if (typeof value === 'string') setValue('date', value)
+              }}
+            />
+          </FormField>
 
-        <Button label={'Submit'} type={'submit'} alignSelf={'center'} />
-      </Box>
-    </form>
+          <Box align={'center'}>
+            <Button
+              label={'Submit'}
+              type={'submit'}
+              style={{ background: 'var(--accent1)', fontWeight: 'bold' }}
+              primary
+            />
+          </Box>
+        </form>
+      </Wrapper>
+    </Main>
   )
 }
 
