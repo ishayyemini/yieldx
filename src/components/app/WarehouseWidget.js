37825657ae317c35580dea39033b12d798e33ceb
Ishay Yemini <ishay.yemini@gmail.com>
2022-09-29 14:27:36 +0200

    Apexcharts with stacked columns

--- src/components/app/WarehouseWidget.js
+++ src/components/app/WarehouseWidget.js
@@ -2,11 +2,11 @@ import { useMemo } from 'react'
 import styled from 'styled-components'
 import { Box } from 'grommet'
 import { useTranslation } from 'react-i18next'
-import { Chart } from 'react-charts'
+import Chart from 'react-apexcharts'
 
 const Wrapper = styled(Box).attrs({
   background: { light: 'brand' },
-  pad: 'small',
+  // pad: 'small',
   round: 'small',
 })`
   //flex-direction: row;
@@ -16,7 +16,7 @@ const Wrapper = styled(Box).attrs({
 `
 
 const WarehouseWidget = ({ kind, warehouses }) => {
-  const { t } = useTranslation()
+  const { t } = useTranslation(null, { keyPrefix: 'warehouseWidget' })
 
   // if (kind === 'House' || kind === 'EggStorage' || kind === 'garbage')
   // TODO calculate eggs today
@@ -24,53 +24,42 @@ const WarehouseWidget = ({ kind, warehouses }) => {
   // if (kind === 'House' || kind === 'EggStorage' || kind === 'Loading Ramp')
   // TODO use sensors
 
-  const data = useMemo(
+  const series = useMemo(
     () => [
       {
-        label: 'React Charts',
-        data: warehouses,
+        name: 'Eggs Today',
+        type: 'bar',
+        data: warehouses.map((wh) => ({ x: wh.Name, y: wh.AmountToday })),
+      },
+      {
+        name: 'Eggs Total',
+        type: 'bar',
+        data: warehouses.map((wh) => ({
+          x: wh.Name,
+          y: wh.AmountTotal - wh.AmountToday,
+        })),
       },
     ],
     [warehouses]
   )
 
-  const primaryAxis = useMemo(
-    () => ({
-      getValue: (datum) => datum.Name,
-      elementType: 'linear',
-    }),
-    []
-  )
-
-  const secondaryAxes = useMemo(
-    () => [
-      {
-        getValue: (datum) => datum.AmountToday,
-        elementType: 'area',
-        min: 0,
-      },
-      {
-        getValue: (datum) => datum.AmountTotal,
-        elementType: 'bar',
-        min: 0,
-      },
-    ],
-    []
-  )
-
   return (
     <Wrapper flex>
-      {t(`warehouseWidget.${kind}`)}
-      {warehouses.length ? (
-        <Chart
-          options={{
-            data,
-            primaryAxis,
-            secondaryAxes,
-            padding: { left: 0, right: 20, bottom: 10 },
-          }}
-        />
-      ) : null}
+      <Chart
+        options={{
+          chart: {
+            id: kind,
+            toolbar: { show: false },
+            stacked: true,
+          },
+          legend: { show: false },
+          title: { text: t(kind) },
+          xaxis: { categories: warehouses.map((item) => item.Name) },
+        }}
+        series={series}
+        width={'100%'}
+        height={'100%'}
+      />
     </Wrapper>
   )
 }
