9fb383601af180f0163d87892580a509055783f5
Ishay Yemini <ishay.yemini@gmail.com>
2022-11-10 13:35:43 +0200

    Added nicer colors and details for ProductView.js

--- src/components/ProductView.js
+++ src/components/ProductView.js
@@ -1,23 +1,69 @@
-import { useContext, useEffect } from 'react'
+import { memo, useContext, useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 import ReactFlow, {
   Background,
   Controls,
+  Handle,
   MiniMap,
   useEdgesState,
   useNodesState,
 } from 'reactflow'
 import 'reactflow/dist/style.css'
+import { Box, Card, Text } from 'grommet'
 
 import GlobalContext from './app/GlobalContext'
 import API from '../data/API'
 
-const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
-]
+const initialNodes = []
+const initialEdges = []
 
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }]
+const whColors = {
+  House: '#a2f9d8',
+  Truck: '#6bcaef',
+  EggStorage: '#fcb0d1',
+  Setter: '#84f98c',
+  HatchRoom: '#c283ef',
+  ChickHall: '#f2e782',
+}
+
+const ProductNode = memo(({ data }) => {
+  const { warehouses } = useContext(GlobalContext)
+  const wh = warehouses[data.DestinationWH] ?? {}
+
+  return (
+    <Card
+      margin={'none'}
+      background={whColors[wh.Type]}
+      width={{ max: '210px' }}
+    >
+      {!data.start ? (
+        <Handle type={'target'} position={'left'} isConnectable={false} />
+      ) : null}
+      <Box border={'bottom'}>
+        <Text weight={'bold'} textAlign={'center'}>
+          {wh.Name}
+        </Text>
+        <Text textAlign={'center'}>
+          {new Date(data.CreateDate).toLocaleString('en-GB', {
+            dateStyle: 'short',
+            timeStyle: 'medium',
+          })}
+        </Text>
+      </Box>
+      <Box>
+        <Text weight={'bold'} textAlign={'center'}>
+          {data.Name} - {data.Amount}
+        </Text>
+      </Box>
+
+      {!data.end ? (
+        <Handle type={'source'} position={'right'} isConnectable={false} />
+      ) : null}
+    </Card>
+  )
+})
+
+const nodeTypes = { custom: ProductNode }
 
 const ProductView = () => {
   const { products } = useContext(GlobalContext)
@@ -31,8 +77,6 @@ const ProductView = () => {
     API.getProductHistory(UID).then()
   }, [UID])
 
-  console.log(product)
-
   const [nodes, setNodes] = useNodesState(initialNodes)
   const [edges, setEdges] = useEdgesState(initialEdges)
 
@@ -41,10 +85,13 @@ const ProductView = () => {
       setNodes(
         product.History.map((item, index, array) => ({
           id: index.toString(),
-          position: { x: index * 200, y: 0 },
-          data: { label: item.Name },
-          targetPosition: index !== 0 ? 'left' : 'none',
-          sourcePosition: index !== array.length - 1 ? 'right' : 'none',
+          position: { x: index * 250, y: 0 },
+          data: {
+            ...item,
+            start: index === 0,
+            end: index === array.length - 1,
+          },
+          type: 'custom',
         }))
       )
       setEdges(
@@ -61,6 +108,7 @@ const ProductView = () => {
     <ReactFlow
       nodes={nodes}
       edges={edges}
+      nodeTypes={nodeTypes}
       nodesDraggable={false}
       nodesConnectable={false}
       nodesFocusable={false}
